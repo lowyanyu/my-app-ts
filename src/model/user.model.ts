c61efb70c25486613fb05a759ad3bb6c38e9a9aa
lowyanyu <lyyyanyu@gmail.com>
2022-06-03 17:49:27 +0800

    adjust user model

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -1,7 +1,7 @@
 export class User {
-	userId: number = NaN;
-	userAccount: string = '';
-	userName: string = '';
+	userId!: number;
+	userAccount!: string;
+	userName!: string;
 
 	constructor(init?: Partial<User>) {
 		Object.assign(this, init);
